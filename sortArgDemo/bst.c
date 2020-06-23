a9a1eefeb430a34ae1f9e7a298cbab3aaa15ca0f
aamartinez24 <aamartinez24@miners.utep.edu>
2020-06-23 09:54:09 -0600

    added helper functions

--- sortArgDemo/bst.c
+++ sortArgDemo/bst.c
@@ -1,23 +1,23 @@
 #include "malloc.h"
 #include "bst.h"
 
-BstNode *root = 0;
+BstNode *bstRoot = 0;           /* bst is empty if root points to zero */
 
 int bstStrcmp(char *a, char *b) /* return pos if string a > string b, 0 if equal, neg otherwise */
 {
-  while (*a && *b) {		/* until end of string */
-    int diff = *a++ - *b++;
+  while (*a && *b) {		/* until end of string (while both operands are non-zero) */
+    int diff = *a++ - *b++;     /* characters are different */
     if (diff) return diff;	/* differed within string */
   }
   if (!*a && !*b) return 0;	/* strings are same length & content */
-  return (*a) ? 1 : -1;		/* a > b if a is longer */
+  return (*a) ? 1 : -1;		/* a > b if a is longer then (+) otherwise (-)  */
 }
 
 int bstStrlen(char *s)
 {
-  char *sc = s;
-  while (*sc++)
-  return sc - s;
+  char *sc = s;                 /* create a pointer that points to the start of the string */
+  while (*sc++)                 /* until the pointer reach the end of the string or equals to 0 */
+    return sc - s;              /* calculate difference of both pointers (lenght of string) */
 }
 
 
@@ -46,19 +46,19 @@ bstInsert(BstNode **rootp, char *str)
 void
 bstPrint(BstNode *root)		/* in order */
 {
-  if (root == 0) return;
-  bstPrint(root->children[0]);
-  puts(root->str); 
-  bstPrint(root->children[1]);
+  if (root == 0) return;        /* root points to 0 (null) */
+  bstPrint(root->children[0]);  /* root points to left child */
+  puts(root->str);              /* prints string that root is pointing */
+  bstPrint(root->children[1]);  /* root points to right child */
 }
 
 void
 bstClear(BstNode **rootp)
 {
-  if (*rootp == 0) return;
+  if (*rootp == 0) return;               /* root is empty do nothing */
   for (int i = 0; i < 2; i++)
-    bstClear(&(*rootp)->children[i]);
-  free((*rootp)->str);
-  free(*rootp);
-  *rootp = 0;
+    bstClear(&(*rootp)->children[i]);    /* iterate thru the children of the root */
+  free((*rootp)->str);                   /* clears (deletes) the string */
+  free(*rootp);                          /* clears the root or bstNode */
+  *rootp = 0;                            /* make root point to null */
 }
---
+++ src/helper.c
@@ -0,0 +1,48 @@
+int space_char(char c)
+{
+  if(c == ' ' || c == '\t')
+    return 1;
+  return 0;
+}
+
+int non_space_char(char c)
+{
+  if(c != ' ' && c != '\t')
+    return 1;
+  return 0;
+}
+
+char word_start(char *str)
+{
+  while(*str){
+    if(non_space_char(str))
+      return *str;
+    *str++;
+  }
+  return 0;
+}
+
+char *word_terminator(char *word)
+{
+  while(*word){
+    if(space_char(word))
+      return *--word;
+    *word++;
+  return;
+}
+  
+int count_words(char *str)
+{
+  int words = 0;
+  int state = 0;
+  while(*str){
+    if(state == 0 && non_space_char(str)){
+      state = 1;
+      ++words;
+    }
+    else if(state == 1 && space_char(str))
+      state = 0;
+    *str++;
+  }
+  return words;
+}
