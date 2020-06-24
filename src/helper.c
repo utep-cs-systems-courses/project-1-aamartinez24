bfc39d49c6f19088f0161ef13aa6f12b6dbf3a80
aamartinez24 <aamartinez24@miners.utep.edu>
2020-06-24 12:02:17 -0600

    fixed helper functions

--- src/helper.c
+++ src/helper.c
@@ -15,7 +15,7 @@ int non_space_char(char c)
 char word_start(char *str)
 {
   while(*str){
-    if(non_space_char(str))
+    if(non_space_char(*str))
       return *str;
     *str++;
   }
@@ -24,10 +24,11 @@ char word_start(char *str)
 
 char *word_terminator(char *word)
 {
-  while(*word){
-    if(space_char(word))
-      return *--word;
-    *word++;
+  char *end = word;
+  while(*end){
+    if(space_char(*end))
+      return *end;
+    *end++;
   return;
 }
   
@@ -36,11 +37,11 @@ int count_words(char *str)
   int words = 0;
   int state = 0;
   while(*str){
-    if(state == 0 && non_space_char(str)){
+    if(state == 0 && non_space_char(*str)){
       state = 1;
       ++words;
     }
-    else if(state == 1 && space_char(str))
+    else if(state == 1 && space_char(*str))
       state = 0;
     *str++;
   }
