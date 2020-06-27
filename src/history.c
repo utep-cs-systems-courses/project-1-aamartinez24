210e56a474460f6fa52b17dcc78c8ea3ba417a9c
aamartinez24 <aamartinez24@miners.utep.edu>
2020-06-27 14:23:03 -0600

    Finished history.c functions

---
+++ src/history.c
@@ -0,0 +1,59 @@
+#include <history.h>
+
+List *init_history()
+{
+  List *head = (List*) malloc(sizeof(List));
+  head->root = 0;
+  return head;
+}
+
+void add_history(List *list, char *str)
+{
+  Item *newItem = (Item*) malloc(sizeof(Item));
+  if(list->root == 0){
+    newItem->id = 1;
+    list->root = newItem;
+  }
+  else{
+    Item *currItem = list->root;
+    while(currItem->next != 0)
+      currItem = currItem->next;
+    newItem->id = currItem->id + 1;
+    currItem->next = newItem;
+  }
+  newItem->str = str;
+  newItem->next = 0;
+}
+
+char *get_history(List *list, int id)
+{
+  Item *currItem = list->root;
+  while(currItem != 0){
+    if(currItem->id == id)
+      return currItem->str;
+    currItem = currItem->next;
+  }
+  printf("id not found");
+  return;
+}
+
+void print_history(List *list)
+{
+  Item *currItem = list->root;
+  while(currItem != 0){
+    printf("id: %d\n", currItem->id);
+    printf("string: ");
+    puts(currItem->str);
+    currItem = currItem->next;
+  }
+}
+
+void free_history(List *list)
+{
+  Item *currItem = list->root;
+  while(currItem != 0){
+    free(currItem);
+    currItem = currItem->next;
+  }
+  free(list);
+}
